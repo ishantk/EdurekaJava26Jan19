b002dd21f56fa3255b55a89cf42ceb9849461a17
Ishant Kumar <er.ishant@gmail.com>
2019-01-26 10:54:47 +0530

    Session1 - Introduction to Java

---
+++ Session1/src/MyApp.java
@@ -0,0 +1,37 @@
+// In Java -> Whatever classes are created in Source Code shall have byte code after compilation
+// 		   -> Its not the source file name getting compiled to a byte code
+//		   -> Its actual class written which is converted to byte code
+public class MyApp {
+
+	// main is a method
+	// whatever we write in main is executed by JVM
+	public static void main(String[] args) {
+		
+		//System.out.println(); -> Built in Code which displays something to User
+		System.out.println("Be Exceptional !!");
+		
+		// a is a single value container
+		// int specifies the type of data i.e. Integral and size i.e. max of 32 bits
+		// 10 is data or value or literal
+		
+		// a, b and c represents MODEL
+		// a*a + b*b + 2*a*b represents CONTROLLER
+		// System.out.println("c is: "+c) represents VIEW
+		int a = 10;
+		int b = 20;
+		int c = a*a + b*b + 2*a*b;
+		System.out.println("c is: "+c);
+
+	}
+
+}
+
+class One {
+	
+}
+
+class Two {
+	
+}
+
+
